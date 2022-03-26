8afb3e69d3096d0d1d84703cd7e68446c72a6fff
Kolanginathan <kolangimail@gmail.com>
2022-03-26 20:43:45 +0530

    javafiledone

---
+++ GreensTech.java
@@ -0,0 +1,58 @@
+package com.Greens;
+
+public class GreensTech {
+	
+	public void greensOmr()
+	{
+		System.out.println("\nGreens Tech - OMR\r\n"+"Plot No.19,\r\n"
+	                       +"Balamurugan Garden,\r\n"
+				           +"OMR Road, Kanchipuram (Dt),\r\n"
+	                       +"Okkiam, Thoraipakkam,\r\n"
+				           +"Jain College Bus Stop.");
+		
+	}
+	public void greensAdayar()
+	{
+		System.out.println("\nGreens Tech - Adayar\r\n"+"No.11,\r\n"
+	                       +"First Street,\r\n"
+				           +"Padmanaba Nagar,\r\n"
+	                       +"Adayar,\r\n"
+				           +"Chennai-600 020.");
+	}
+	public void greensTambaram()
+	{
+		System.out.println("\nGreens Tech - Tambaram\r\n"+"No.1, Apparao Colony,\r\n"
+	                       +"Sanitorium,\r\n"
+				           +"Tambaram,\r\n"
+	                       +"Near HP Petrol Bunk,\r\n"
+				           +"Chennai-600 047.");
+	}
+	public void greensPorur()
+	{
+		System.out.println("\nGreens Tech - Porur\n"+"149, 1C/1D,\r\n"
+	                       +"1st FLoor,\r\n"
+				           +"Oppo to DLF IT Park,\r\n"
+	                       +"Ramapuram,\r\n"
+				           +"Chennai-600 089.");
+	}
+	public void greensPerumpakkam()
+	{
+		System.out.println("\nGreens Tech - Perumpakkam\n"+"No. 19 & 20,\r\n"
+	                       +"1st FLoor,\r\n"
+				           +"American Advent Christian Layout,\r\n"
+	                       +"Sholinganallur to Medavakkam Main Road,\r\n"
+				           +"Perumbakkam,\r\n"
+				           +"Chennai-600 089.");
+	}
+	public static void main(String[] args) {
+		GreensTech G=new GreensTech();
+		G.greensPorur();
+		G.greensTambaram();
+		G.greensAdayar();
+		G.greensOmr();
+		G.greensPerumpakkam();
+		
+
+	}
+
+}
